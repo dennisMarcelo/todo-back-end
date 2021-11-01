fd101bd406221fc769266bdf496b5f74e52e9c4e
dennisMarcelo <dennismarcelo15@gmail.com>
2021-11-01 17:28:40 -0300

    defined struct of userController tests

---
+++ src/tests/unitTests/userController.test.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const userController = require('../../controllers/userController')
+
+describe('userController', () => {
+  describe('route POST /user', () => {
+    it('return staus 201', () => {
+
+    })
+
+    it('return a json with one message and token', async () => {
+      
+    });
+  });
+
+  describe('route POST /user/login', () => {
+    it('return stutus 200', async ()=>{
+
+    });
+
+    it('return a json with one message and token',() => {
+
+    });
+  });
+});
