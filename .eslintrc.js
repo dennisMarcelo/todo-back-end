bb2e57f3f5b1f37d0f741ca7e3b5323b1f08a76c
dennisMarcelo <dennismarcelo15@gmail.com>
2021-10-31 22:54:54 -0300

    fixing eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,67 +1,15 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "airbnb-base"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 13
-    },
-    rules: {
-        'no-console': 'off',
-        camelcase: [
-          2,
-          {
-            ignoreDestructuring: true,
-          },
-        ],
-        'arrow-parens': [
-          2,
-          'always',
-        ],
-        quotes: [
-          2,
-          'single',
-        ],
-        'implicit-arrow-linebreak': 'off',
-        'consistent-return': 'off',
-        'no-unused-vars': [
-          'error',
-          {
-            argsIgnorePattern: '^_',
-            ignoreRestSiblings: true,
-          },
-        ],
-        'object-curly-newline': 'off',
-        'max-params': [
-          'error',
-          4,
-        ],
-        'max-lines': [
-          'error',
-          250,
-        ],
-        'max-lines-per-function': [
-          'error',
-          {
-            max: 20,
-            skipBlankLines: true,
-            skipComments: true,
-          },
-        ],
-        'max-len': [
-          'error',
-          {
-            code: 100,
-            ignoreComments: true,
-          },
-        ],
-        complexity: [
-          'error',
-          5,
-        ],
-      },
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+  },
 };
