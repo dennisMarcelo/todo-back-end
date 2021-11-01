3cd36f13460afba1496401e8fa7828e8bee4fce6
dennisMarcelo <dennismarcelo15@gmail.com>
2021-11-01 17:21:39 -0300

    unnecessary line removed

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,10 +5,8 @@ const router = express.Router();
 
 const userService = require('../services/userService');
 
-router.post('/', rescue(async (req, res, next) => {
+router.post('/', rescue(async (req, res) => {
   const token = await userService.create(req.body);
-
-  if (!token) return next({ message: 'User not registered, try again.' });
 
   res.status(201).json({ message: 'User registered successfully', token });
 }));
