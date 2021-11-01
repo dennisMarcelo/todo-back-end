ba06a25d9792195f1df871ecf11df21f862fcf52
dennisMarcelo <dennismarcelo15@gmail.com>
2021-11-01 11:41:21 -0300

    user Service return now a user token

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,12 +1,16 @@
 const isValid = require('../helpers/validateFields');
+const { tokenGenerator } = require('../helpers/token');
 
 const userModel = require('../models/userModel');
 
 const create = async (newUser) => {
   isValid.newUser(newUser);
-  const userRegistered = await userModel.create(newUser);
+  const id = await userModel.create(newUser);
+  const { name, email } = newUser;
 
-  return userRegistered;
+  const token = tokenGenerator({ id, name, email });
+
+  return token;
 };
 
 module.exports = {
