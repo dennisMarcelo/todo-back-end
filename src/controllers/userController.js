b2d8242c865887c17055461d19bdc63da720c63f
dennisMarcelo <dennismarcelo15@gmail.com>
2021-11-01 10:54:59 -0300

    fixing the parameter type in the next()

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,7 +8,7 @@ const userService = require('../services/userService');
 router.post('/', rescue(async (req, res, next) => {
   const userRegistered = await userService.create(req.body);
 
-  if (!userRegistered) return next('Error, user not registered');
+  if (!userRegistered) return next({ message: 'User not registered, try again.' });
 
   res.status(201).json({ message: 'Success' });
 }));
