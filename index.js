dc40336b6e110afd68dc2ed1174c61aaf0818926
dennisMarcelo <dennismarcelo15@gmail.com>
2021-11-01 10:36:38 -0300

    making available route to user in index

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+// controllers
+const userController = require('./src/controllers/userController');
+
 // config
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// routes
+app.use('/user', userController);
 
 app.listen(PORT, () => console.log('servidor online'));
