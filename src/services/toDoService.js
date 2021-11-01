9bf08690a3bfc4cd5f91fc3fac6cfee97609fe90
dennisMarcelo <dennismarcelo15@gmail.com>
2021-11-01 20:53:36 -0300

    newToDO validation added in newToDOService

--- src/services/toDoService.js
+++ src/services/toDoService.js
@@ -1,7 +1,9 @@
 const getDateFormated = require('../helpers/getDateFormated');
 const toDoModel = require('../models/toDoModel');
+const isValid = require('../helpers/validateFields');
 
 const create = async (newToDo, user) => {
+  isValid.newToDo(newToDo);
   const { userId } = user;
   const toDo = await toDoModel.create({ ...newToDo, userId, creationDate: getDateFormated() });
 
