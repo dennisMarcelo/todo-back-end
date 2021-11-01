a8468ff7497aca9bc25c08978257a3a1a77acb35
dennisMarcelo <dennismarcelo15@gmail.com>
2021-11-01 19:17:28 -0300

    function create added in toDoModel

---
+++ src/models/toDoModel.js
@@ -0,0 +1,22 @@
+const mongoConnection = require('./connection');
+
+const getConnectionWithCollection = async () => {
+  const connnectionWithCollection = await mongoConnection.getConnection()
+    .then((db) => db.collection('toDos'));
+
+  return connnectionWithCollection;
+};
+
+const create = async ({ userId, toDo, toDoStatus, creationDate }) => {
+  const collection = getConnectionWithCollection();
+  const { insertedId: id } = await collection
+    .insertOne({ userId, toDo, toDoStatus, creationDate });
+
+  return {
+    id, userId, toDo, toDoStatus, creationDate,
+  };
+};
+
+module.exports = {
+  create,
+};
