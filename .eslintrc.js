932f91d7af5189a22c12efbcd83c282dc4c515eb
dennisMarcelo <dennismarcelo15@gmail.com>
2021-10-31 22:57:14 -0300

    fixing Roles eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,57 @@ module.exports = {
     ecmaVersion: 12,
   },
   rules: {
+    'no-console': 'off',
+    camelcase: [
+      2,
+      {
+        ignoreDestructuring: true,
+      },
+    ],
+    'arrow-parens': [
+      2,
+      'always',
+    ],
+    quotes: [
+      2,
+      'single',
+    ],
+    'implicit-arrow-linebreak': 'off',
+    'consistent-return': 'off',
+    'no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
+    'object-curly-newline': 'off',
+    'max-params': [
+      'error',
+      4,
+    ],
+    'max-lines': [
+      'error',
+      250,
+    ],
+    'max-lines-per-function': [
+      'error',
+      {
+        max: 20,
+        skipBlankLines: true,
+        skipComments: true,
+      },
+    ],
+    'max-len': [
+      'error',
+      {
+        code: 100,
+        ignoreComments: true,
+      },
+    ],
+    complexity: [
+      'error',
+      5,
+    ],
   },
 };
