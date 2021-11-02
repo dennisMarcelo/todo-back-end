9b67d200969ab0b8c02a328e2eacef41531cebe1
dennisMarcelo <dennismarcelo15@gmail.com>
2021-11-01 23:50:04 -0300

    function remove added in toDoService

--- src/services/toDoService.js
+++ src/services/toDoService.js
@@ -1,6 +1,7 @@
 const getDateFormated = require('../helpers/getDateFormated');
 const toDoModel = require('../models/toDoModel');
 const isValid = require('../helpers/validateFields');
+const CustomError = require('../helpers/CustomError');
 
 const create = async (newToDo, user) => {
   isValid.newToDo(newToDo);
@@ -16,7 +17,19 @@ const findAll = async (userId) => {
   return toDos;
 };
 
+const remove = async (id, user) => {
+  const toDo = await toDoModel.findById(id);
+  if (!toDo) throw new CustomError('Not exist this to-do');
+  if (toDo.userId !== user.id) {
+    throw new CustomError('You not have authorization to remove this to-do', 401);
+  }
+
+  const isRemoved = await toDoModel.remove(id);
+  return isRemoved;
+};
+
 module.exports = {
   create,
   findAll,
+  remove,
 };
