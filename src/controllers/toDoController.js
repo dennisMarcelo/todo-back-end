3ef3c3935dfd9a0842fc02bb217922ca16106409
dennisMarcelo <dennismarcelo15@gmail.com>
2021-11-01 23:50:39 -0300

    route remove added in toDoControler

--- src/controllers/toDoController.js
+++ src/controllers/toDoController.js
@@ -19,4 +19,13 @@ router.get('/', validateJWT, rescue(async (req, res) => {
   res.status(200).json({ toDos });
 }));
 
+router.delete('/:id', validateJWT, rescue(async (req, res) => {
+  const { id } = req.params;
+
+  const isRemoved = await toDoService.remove(id, req.user);
+  if (!isRemoved) res.status(500).json({ message: 'Not removed' });
+
+  return res.status(204).json({ message: 'To-do removed' });
+}));
+
 module.exports = router;
